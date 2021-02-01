449d25cf7c0107e2b6c69e293ae76cd2c3b75e8a
Sungmin Woo <woos8899@gmail.com>
2021-02-01 23:38:48 +0900

    Better looking post list, with pagination

--- web/src/components/announce/AnnounceList.tsx
+++ web/src/components/announce/AnnounceList.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { List, Button } from 'semantic-ui-react';
+import { Menu, Button, Table, Label, Icon } from 'semantic-ui-react';
 import { Announcement, fetchAnnounceList } from './client';
 
 interface AnnounceProps {
@@ -8,34 +8,96 @@ interface AnnounceProps {
 };
 interface AnnounceStats {
     announceList : Announcement[]
+
+    listPageSize: number;
+    listCurrentPage: number;
 };
 
 export class AnnounceList extends React.Component<AnnounceProps, AnnounceStats> {
     constructor(props: AnnounceProps) {
         super(props);
         this.state = {
-            announceList: []
+            announceList: [],
+
+            listPageSize: 3,
+            listCurrentPage: 0,
         };
     }
 
     public render() {
-        console.log(this.state.announceList);
-        const listItems = this.state.announceList.map(Announcement =>
-            <List.Item
-                key = {Announcement.id}
-                as = {NavLink}
-                to = {`/announcements/page/${Announcement.id}`}
-            >
-                {Announcement.title}
-                {Announcement.date}
-                {Announcement.writer.name}
-            </List.Item>
-        );
+        const tableRows: JSX.Element[] = [];
+        const begin_index = this.state.listPageSize * this.state.listCurrentPage;
+        const end_index = this.state.listPageSize * (this.state.listCurrentPage + 1);
+        for (let i = begin_index; i < end_index; ++i) {
+            const one_element = this.state.announceList[i];
+            if (undefined === one_element) {
+                continue;
+            }
+
+            tableRows.push(
+                <Table.Row key={`announce_row_${one_element.id}`}>
+                    <Table.Cell>
+                        <Label
+                            key = {one_element.id}
+                            as = {NavLink}
+                            to = {`/announcements/page/${one_element.id}`}
+                        >
+                            {one_element.title}
+                        </Label>
+                    </Table.Cell>
+                    <Table.Cell>{one_element.writer.name}</Table.Cell>
+                    <Table.Cell>{one_element.date}</Table.Cell>
+                </Table.Row>
+            )
+        }
+
+        const paginatorBtnList: JSX.Element[] = [];
+        const totalPagesCount = Math.ceil(this.state.announceList.length / this.state.listPageSize);
+        for (let i = 0; i < totalPagesCount; ++i) {
+            paginatorBtnList.push(
+                <Menu.Item
+                    as='a'
+                    name={`page_btn_${i}`}
+                    active={this.state.listCurrentPage === i}
+                    onClick={() => this.setCurrentListPage(i)}
+                >{i + 1}</Menu.Item>
+            );
+        }
 
         return  (
             <div>
                 <h1>공지 사항</h1>
-                <List items = {listItems}/>
+
+                <Table celled>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>제목</Table.HeaderCell>
+                            <Table.HeaderCell>글쓴이</Table.HeaderCell>
+                            <Table.HeaderCell>날짜</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>{tableRows}</Table.Body>
+
+                    <Table.Footer>
+                        <Table.Row>
+                            <Table.HeaderCell colSpan='3'>
+                            <Menu floated='right' pagination>
+                                <Menu.Item as='a' icon>
+                                    <Icon name='chevron left' />
+                                </Menu.Item>
+
+                                {paginatorBtnList}
+
+                                <Menu.Item as='a' icon>
+                                    <Icon name='chevron right' />
+                                </Menu.Item>
+                            </Menu>
+                            </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Footer>
+                </Table>
+
                 <input id = "search"></input>
                 <button id = "searchBtn">검색</button>
                 <Button
@@ -49,8 +111,8 @@ export class AnnounceList extends React.Component<AnnounceProps, AnnounceStats> 
 
     public componentDidMount() {
         this.fetchList();
-        this.state.announceList.forEach(element => {   
-            console.log("writer: ", element.writer); 
+        this.state.announceList.forEach(element => {
+            console.log("writer: ", element.writer);
         });
     }
 
@@ -64,4 +126,11 @@ export class AnnounceList extends React.Component<AnnounceProps, AnnounceStats> 
                 console.log(err);
             });
     }
+
+    private setCurrentListPage(pageIndex: number) {
+        this.setState({
+            listCurrentPage: pageIndex,
+        });
+    }
+
 }
